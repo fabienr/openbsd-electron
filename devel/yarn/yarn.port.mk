48059b41359977cb90420655d666053db1ca97c9
Fabien Romano <fabienromano@gmail.com>
2024-06-24 18:57:26 +0100

    MODYARN need PORTHOME?=${WRKDIR}. While there, adjust MODYARN_OMITOPTIONAL comments.

--- devel/yarn/yarn.port.mk
+++ devel/yarn/yarn.port.mk
@@ -4,7 +4,7 @@ MODYARN_CACHE?=		${WRKDIR}/.yarn
 MODYARN_TARGETS?=	${WRKSRC}
 MODYARN_LOCKS?=		${MODYARN_TARGETS:=%/yarn.lock}
 
-MODYARN_OMITOPTIONAL?=	No # omit optional depends
+MODYARN_OMITOPTIONAL?=	No # if Yes, omit optional depends
 
 SITES.yarn?=		https://registry.yarnpkg.com/
 SITES.yarn_npm?=	https://registry.npmjs.org/
@@ -20,6 +20,8 @@ TEMPLATE_DISTFILES.yarn_codeload?= \
 	${MODYARN_DIST}/${TEMPLATE_DISTFILES.github:S/-{/%{/}
 
 BUILD_DEPENDS+=		devel/yarn
+
+PORTHOME?=		${WRKDIR}
 
 EXTRACT_CASES+=		${MODYARN_DIST}/*.git-*) \
 	_filename=$$(echo $${archive\#\#*/} | \
@@ -43,7 +45,6 @@ EXTRACT_CASES+=		${MODYARN_DIST}/*) \
 	;;
 
 # XXX would be easier for porters if DIST_TUPLE_post-extract ran before
-# XXX --ignore-optional ?
 .if empty(_GEN_MODULES)
 MODYARN_post-extract+= \
 	if [ -e ${MODYARN_CACHE} ]; then \
@@ -73,7 +74,7 @@ modyarn-gen-modules:
 		${MODYARN_LOCKS}
 .endif
 
-# XXX not the proper deisgn yet
+# XXX not the proper design yet
 # .if !target(modyarn-vendor-cache)
 # modyarn-vendor-cache:
 # 	@if [ ! -d ${WRKSRC} ]; then ${_MAKE} extract >/dev/null 2>&1 ; fi
