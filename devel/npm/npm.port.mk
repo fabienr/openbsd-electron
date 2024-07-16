1a69f66d92142f4e5272db484625b4a9a05d8c60
Fabien Romano <fabienromano@gmail.com>
2024-07-16 22:28:28 +0100

    update MODNPM, handle multiple locks, simplify extract, cp, rm logic, bring more coherency with MODYARN.

--- devel/npm/npm.port.mk
+++ devel/npm/npm.port.mk
@@ -1,53 +1,50 @@
 MODNPM_DIST=	npm_modules
 
+MODNPM_CACHE?=		${WRKDIR}/.npm
+MODNPM_TARGETS?=	${WRKSRC}
+MODNPM_LOCKS?=		${MODNPM_TARGETS:=%/package-lock.json}
+
 MODNPM_OMITDEV?=	No # omit dev depends
 MODNPM_OMITOPTIONAL?=	No # omit optional depends
 MODNPM_INCLUDES?= 	# include package
 MODNPM_EXCLUDES?= 	# exclude package
-# XXX MODNPM_LOCK(s), handle one file only
-MODNPM_LOCKS ?=		${WRKSRC}/package-lock.json
 
 MODNPM_NPM_MOD=${LOCALBASE}/lib/node_modules/npm
 MODNPM_GYP_BIN=${MODNPM_NPM_MOD}/node_modules/node-gyp/bin/node-gyp.js
 
 SITES.npm ?=	https://registry.npmjs.org/
 
+# XXX use DISTFILES.npm instead of DIST_TUPLE ? (get rid of .- maybe)
 TEMPLATE_DISTFILES.npm ?= \
 	${MODNPM_DIST}/<account>-{<account>/<project>/-/}<project>-<id>.tgz
 
-# XXX use FIX_EXTRACT_PERMISSIONS instead of chmod ? need to (re)find culprit
+# XXX always fix permission, at least for pngjs-6.0.0 (from games/byar-chobby)
 EXTRACT_CASES +=	${MODNPM_DIST}/*.tgz) \
-	_filename=$${archive\#\#*/} && \
+	_filename=$${archive\#\#*/} ; \
+	_location=${MODNPM_CACHE}/$${_filename%.tgz} ; \
+	[[ -d ${MODNPM_CACHE} ]] || mkdir ${MODNPM_CACHE} ; \
 	${GZIP_CMD} -d <${FULLDISTDIR}/$$archive | ${TAR} -xf - -- && \
 	mv "`${TAR} -ztf ${FULLDISTDIR}/$$archive | \
-		awk -F/ '{print $$1}' | uniq`" $${_filename%.tgz} && \
-	chmod -R a+x $${_filename%.tgz};;
+		awk -F/ '{print $$1}' | uniq`" $$_location && \
+	chmod -R a+rX $$_location ;;
 
-.if !empty(MODNPM_MV)
-.  for _src _dst in ${MODNPM_MV}
+.if !empty(MODNPM_CP) && empty(_GEN_MODULES)
+# note _dst;_src because list is sorted to let shorter _dst path come first
 MODNPM_post-extract += \
-	t=`dirname ${WRKSRC}/${_dst}`; \
-	[[ -d $$t ]] || mkdir -p $$t ; \
-	mv ${WRKDIR}/${_src} ${WRKSRC}/${_dst} ;
-.  endfor
+	for _cp in $$(echo "${MODNPM_CP}"); do \
+		_dst=$${_cp%;*} ; _src=$${_cp\#*;} ; \
+		_t=`dirname ${WRKSRC}/$${_dst}`; \
+		[[ -d $$_t ]] || mkdir -p $$_t ; \
+		cp -Rp ${WRKDIR}/.npm/$${_src} ${WRKSRC}/$${_dst} ; \
+	done ;
 .endif
 
-.if !empty(MODNPM_CP)
-.  for _dst _src in ${MODNPM_CP}
-MODNPM_post-extract += \
-	t=`dirname ${WRKSRC}/node_modules/${_dst}`; \
-	[[ -d $$t ]] || mkdir -p $$t ; \
-	cp -Rp ${WRKSRC}/${_src} ${WRKSRC}/node_modules/${_dst} ;
-.  endfor
-.endif
-
-MODNPM_post-extract += rm -rf ${WRKSRC}/.npm ;
+MODNPM_post-extract += rm -rf ${MODNPM_CACHE} ;
 
 # XXX path to npm_dist
-# XXX implement MODNPM_LOCKS (multiple files) ?
 .if !target(modnpm-gen-modules)
 modnpm-gen-modules:
-	@if [ ! -d ${WRKSRC} ]; then ${_MAKE} extract >/dev/null 2>&1 ; fi
+	@${_MAKE} -D _GEN_MODULES extract >/dev/null 2>&1
 	# make modnpm-gen-modules > modules.inc
 	# MODNPM_OMITDEV=${MODNPM_OMITDEV}
 	# MODNPM_OMITOPTIONAL=${MODNPM_OMITOPTIONAL}
@@ -58,5 +55,5 @@ modnpm-gen-modules:
 		${MODNPM_OMITOPTIONAL:S/Yes/-o/:S/No//} \
 		${MODNPM_INCLUDES:='-i %'} \
 		${MODNPM_EXCLUDES:='-x %'} \
-		${MODNPM_LOCKS}
+		${WRKSRC} ${MODNPM_LOCKS}
 .endif
