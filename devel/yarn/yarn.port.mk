6af58d00ebaf6fac9eb08d8221fff83aec93a8a5
Fabien Romano <fabienromano@gmail.com>
2024-08-26 21:43:04 +0100

    Move MODYARN bin into proper place. Handle conflict with MODNPM. Implement proper DISTFILES without DIST_TUPLE.

--- devel/yarn/yarn.port.mk
+++ devel/yarn/yarn.port.mk
@@ -1,55 +1,55 @@
 MODYARN_DIST=		yarn_modules
+MODNPM_DIST?=		npm_modules
 
 MODYARN_CACHE?=		${WRKDIR}/.yarn
 MODYARN_TARGETS?=	${WRKSRC}
 MODYARN_LOCKS?=		${MODYARN_TARGETS:=%/yarn.lock}
 
-SITES.yarn?=		https://registry.yarnpkg.com/
-SITES.yarn_npm?=	https://registry.npmjs.org/
-SITES.yarn_github?=	${SITES.github}
-SITES.yarn_codeload?=	${SITES.github}
+MODYARN_OMITOPTIONAL?=	No # omit optional depends
 
-# XXX use DISTFILES.<site> instead of DIST_TUPLE ? (get rid of .- maybe)
-TEMPLATE_DISTFILES.yarn?= \
-	${MODYARN_DIST}/<account>-{<account>/<project>/-/}<project>-<id>.tgz
-TEMPLATE_DISTFILES.yarn_npm = ${TEMPLATE_DISTFILES.yarn}
-TEMPLATE_DISTFILES.yarn_github?= \
-	${MODYARN_DIST}/${TEMPLATE_DISTFILES.github:S/>-</>%</:S/-{/.git-{/}
-TEMPLATE_DISTFILES.yarn_codeload?= \
-	${MODYARN_DIST}/${TEMPLATE_DISTFILES.github:S/-{/%{/}
+SITES.yarn?=		https://registry.yarnpkg.com/
+SITES.npm?=		https://registry.npmjs.org/
 
 BUILD_DEPENDS+=		devel/yarn
 
-PORTHOME?=		${WRKDIR}
+# don't extract, avoid conflict with MODNPM, see post-extract
+EXTRACT_CASES+=		${MODNPM_DIST}/*.tgz) ;;
+EXTRACT_CASES+=		${MODYARN_DIST}/*) ;;
 
-EXTRACT_CASES+=		${MODYARN_DIST}/*.git-*) \
+EXTRACT_GITHUB=		${MODYARN_DIST}/*.git-*) \
 	_filename=$$(echo $${archive\#\#*/} | \
 		sed -e '/%/s/${EXTRACT_SUFX.github}//' \
 		-e '/%/s/.*%//' -e 's/\.-//') ; \
 	_module=$$(${TAR} -ztf ${FULLDISTDIR}/$$archive | \
 		awk -F/ '{print $$1}' | uniq) ; \
-	[[ -d ${MODYARN_CACHE} ]] || mkdir -p ${MODYARN_CACHE} ; \
 	${GZIP_CMD} -d <${FULLDISTDIR}/$$archive | ${TAR} -xf - -- && \
 	${TAR} -cf ${MODYARN_CACHE}/$$_filename -C $$_module ./ && \
 	rm -rf $$_module \
 	;;
-
-EXTRACT_CASES+=		${MODYARN_DIST}/*) \
+EXTRACT_YARN=		${MODYARN_DIST}/*|${MODNPM_DIST}/*.tgz) \
 	_filename=$$(echo $${archive\#\#*/} | \
 		sed -e '/%/s/${EXTRACT_SUFX.github}//' \
-		-e '/%/s/.*%//' -e 's/\.-//') && \
-	mkdir -p ${MODYARN_CACHE} && \
+		-e '/%/s/.*%//' -e 's/\.-//') ; \
 	ln -fs ${FULLDISTDIR}/$$archive \
 		${MODYARN_CACHE}/$$_filename \
 	;;
 
 .if empty(_GEN_MODULES)
+MODYARN_post-extract += \
+	PATH=${PORTPATH}; set -e; cd ${WRKDIR}; \
+	[[ -d ${MODYARN_CACHE} ]] || mkdir -p ${MODYARN_CACHE}; \
+	for archive in ${EXTRACT_ONLY}; do \
+		case $$archive in \
+		${EXTRACT_GITHUB} \
+		${EXTRACT_YARN} \
+		esac; \
+	done ;
 MODYARN_post-extract+= \
 	if [ -e ${MODYARN_CACHE} ]; then \
 		echo 'yarn-offline-mirror "${MODYARN_CACHE}"' >> \
-			${PORTHOME}/.yarnrc ; \
+			${WRKDIR}/.yarnrc ; \
 		for _target in ${MODYARN_TARGETS}; do \
-			cd $$_target && ${SETENV} ${MAKE_ENV} \
+			cd $$_target && ${SETENV} ${MAKE_ENV} HOME=${WRKDIR} \
 			yarn install --frozen-lockfile --offline \
 			--ignore-scripts --ignore-engines \
 			${MODYARN_OMITOPTIONAL:S/Yes/--ignore-optional/:S/No//}\
@@ -60,11 +60,9 @@ MODYARN_post-extract+= \
 
 MODYARN_post-extract += rm -rf ${MODYARN_CACHE} ;
 
-# XXX path to yarn_dist
 .if !target(modyarn-gen-modules)
 modyarn-gen-modules:
 	@make -D _GEN_MODULES extract >/dev/null 2>&1
 	# make modyarn-gen-modules > modules.inc
-	@/usr/ports/mystuff/devel/yarn/yarn_dist \
-		${MODYARN_LOCKS}
+	@${_PERLSCRIPT}/modyarn-gen-modules ${MODYARN_LOCKS}
 .endif
